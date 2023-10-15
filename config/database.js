125c7544f5e59fda132671e8c27f165fe3f6603e
Antonio Barrios <acbleguizamon@gmail.com>
2023-10-15 10:16:22 -0300

    Subida de prueba

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 // Configuracion para conectar a la base de datos 
+// esto es un comentario para realizar prueba de subida
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
